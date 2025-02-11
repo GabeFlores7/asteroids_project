4533d88ca72caff299d8e79fc3fd959422c513ac
GabeFlores7 <gabe7.flores@gmail.com>
2025-02-10 19:56:05 -0600

    Draw Player Completion

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 def main():
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
@@ -11,11 +13,13 @@ def main():
 	background_color = (0,0,0)
 	clock = pygame.time.Clock()
 	dt = 0
+	p1 = Player(x = SCREEN_WIDTH / 2 , y = SCREEN_HEIGHT / 2)
 	while True:
 		for event in pygame.event.get():
     			if event.type == pygame.QUIT:
         			return
 		screen.fill(background_color)
+		p1.draw(screen)
 		pygame.display.flip()
 		dt = clock.tick(60)/1000
 if __name__ == "__main__":
