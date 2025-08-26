e8ad3e13c657383d9ae324506c82a590a3160fab
GabeFlores7 <gabe7.flores@gmail.com>
2025-08-26 15:32:30 -0500

    enable player rotation

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ def main():
 
     pygame.init() #initialize pygame
     clock = pygame.time.Clock() # initialize object to keep track of time
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # create object for display
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) # initialize player obj to screen's center
+    dt = 0
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -25,9 +25,12 @@ def main():
 
         screen.fill((0,0,0)) # make the screen black (r,g,b)
         player.draw(screen) # draw player to screen
+
+        player.update(dt) # update player behavior 
         pygame.display.flip() # update screen
+
         clock.tick(60) # pause loop for 1/60th of a second (i.e. 60fps)
-        dt = clock.tick() / 1000
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -7,6 +7,20 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+
+    def draw(self, screen): # method for drawing player to screen
+        pygame.draw.polygon(screen, "white", self.triangle(), width = 2)
+
+    def rotate(self, dt): # method for rotating player
+        self.rotation += PLAYER_TURN_SPEED * dt
+    
+    def update(self, dt): # method used to control player obj via key inputs
+        keys = pygame.key.get_pressed()
+        # Make game compatible for full sized or compact keyboards
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+            self.rotate(-dt)
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            self.rotate(dt)
     
     # in the player class
     def triangle(self):
@@ -16,6 +30,3 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
-
-    def draw(self, screen): # method for drawing player to screen
-        pygame.draw.polygon(screen, "white", self.triangle(), width = 2)
