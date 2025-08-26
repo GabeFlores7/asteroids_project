a0cdb878d2d730a2497b1d8bc8be238f9926114f
GabeFlores7 <gabe7.flores@gmail.com>
2025-08-26 15:07:01 -0500

    draw player

--- main.py
+++ main.py
@@ -3,22 +3,30 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
-    pygame.init()
-    clock = pygame.time.Clock()
+
+    pygame.init() #initialize pygame
+    clock = pygame.time.Clock() # initialize object to keep track of time
     dt = 0
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # create object for display
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) # initialize player obj to screen's center
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    while True:
-        for event in pygame.event.get():
+
+    while True: # run game indefinitely
+
+        for event in pygame.event.get(): # check if user exits
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
-        pygame.display.flip()
-        clock.tick(60)
+
+        screen.fill((0,0,0)) # make the screen black (r,g,b)
+        player.draw(screen) # draw player to screen
+        pygame.display.flip() # update screen
+        clock.tick(60) # pause loop for 1/60th of a second (i.e. 60fps)
         dt = clock.tick() / 1000
 
 if __name__ == "__main__":
